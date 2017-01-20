9498f69e93a188496cd6071f37ced29f78a63bd9
Tist0 <jerome6500@gmail.com>
2017-01-20 17:15:25 +0100

    avancé mineur partie

--- src/main/java/perudo/partie.java
+++ src/main/java/perudo/partie.java
@@ -8,6 +8,21 @@
  *
  * @author jerome
  */
-public class partie {
-    
+public class Partie {
+    private int nbJoueur;
+    private int nbManche;
+    private String listeJoueur; /*a modifier avec la class*/
+    private String nomPartie;
 }
+
+/*Constructeur*/
+public Partie(String nomPartie,int nbJoueur)
+{
+    this.nomPartie = nomPartie;
+    this.nbJoueur = nbJoueur;
+    nbManche = 0;
+    listeJoueur = o;
+}
+
+/**/
+
