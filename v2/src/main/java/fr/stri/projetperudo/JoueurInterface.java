a2554093ebad2a18fcea88e02faaff540bb35c88
Ekinoxies <robinaubert@outlook.fr>
2017-01-23 16:10:18 +0100

    Ajout classe Joueurs + modif Partie

--- v2/src/main/java/fr/stri/projetperudo/JoueurInterface.java
+++ v2/src/main/java/fr/stri/projetperudo/JoueurInterface.java
@@ -3,7 +3,7 @@ package fr.stri.projetperudo;
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
- * and open the template in the editor. CHATS
+ * and open the template in the editor. CHATS fjh:dshdjk
  */
 
 /**
---
+++ v2/src/main/java/fr/stri/projetperudo/Joueurs.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.stri.projetperudo;
+
+/**
+ *
+ * @author Quizz
+ */
+public class Joueurs {
+    public String nomJoueurs;
+    private int nbDes;
+    public int ListeDes;
+
+    public String getNomJoueurs() {
+        return nomJoueurs;
+    }
+    
+}
