fb9887a42502a4ba83501f2664e1fb0e08aa2678
Tist0 <jerome6500@gmail.com>
2017-01-24 16:55:43 +0100

    Classe Connexion réalisé
    L'ordonanceur a etait retiré on etait vraiment pas sur la bonne idée de
    concéption.
    On peut finir vendredi je pense  Aller Bonne chance.

---
+++ v2/src/main/java/fr/stri/projetperudo/Partie.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.stri.projetperudo;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author jerome
+ */
+public class Partie {
+    
+    /* Arguments */
+    private String nomPartie;
+    private  int nbJoueur;
+    private ArrayList<Joueurs> listeJoueur = new ArrayList<Joueurs>();
+
+    
+    /*Constructeur*/
+    
+    public Partie(String nomPartie, int nbJoueur) 
+    {
+    this.nomPartie=nomPartie;
+    this.nbJoueur = nbJoueur;     
+    }
+    
+    
+    
+   /*Methode*/
+    public String connexion (Joueurs a)
+    {
+        String nomJ;
+        listeJoueur.add(a);  
+        nomJ= a.getNomJoueurs();
+        return "Le joueurs nomJ ";
+    }
+   
+    public String avantManche ()
+    {
+    
+       for(int i = 0; i <nbJoueur; i++)
+       {
+           /*get de truck */
+       }
+        
+        return"";
+    }
+   
+    
+    
+    
+    }      
+       
+            
+}
